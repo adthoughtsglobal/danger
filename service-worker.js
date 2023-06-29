efae1b38275a42e16cda94946fba913ae8e4f949
adthoughtsglobal <123654929+adthoughtsglobal@users.noreply.github.com>
2023-06-29 13:08:21 +0530

    even more better

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,34 @@
 // Establish WebSocket connection
-const socket = new WebSocket('wss://example.com/ws');
+let socket;
+
+function connectWebSocket() {
+  socket = new WebSocket('wss://example.com/ws');
+
+  // WebSocket event listeners
+  socket.addEventListener('open', function() {
+    console.log('WebSocket connection established');
+  });
+
+  socket.addEventListener('message', function(event) {
+    console.log('WebSocket message received:', event.data);
+
+    // Check for trigger message
+    if (event.data === 'triggerMessage') {
+      // Send trigger received message to the client
+      self.clients.matchAll().then(function(clients) {
+        clients.forEach(function(client) {
+          client.postMessage('triggerReceived');
+        });
+      });
+    }
+  });
+
+  socket.addEventListener('close', function() {
+    console.log('WebSocket connection closed. Reconnecting...');
+    // Reconnect after a delay
+    setTimeout(connectWebSocket, 5000);
+  });
+}
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -30,31 +59,9 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
-// WebSocket event listeners
-socket.addEventListener('open', function() {
-  console.log('WebSocket connection established');
-});
-
-socket.addEventListener('message', function(event) {
-  console.log('WebSocket message received:', event.data);
-
-  // Check for trigger message
-  if (event.data === 'triggerMessage') {
-    // Send trigger received message to the client
-    self.clients.matchAll().then(function(clients) {
-      clients.forEach(function(client) {
-        client.postMessage('triggerReceived');
-      });
-    });
-  }
-});
-
-socket.addEventListener('close', function() {
-  console.log('WebSocket connection closed. Reconnecting...');
-  // Reconnect after a delay
-  setTimeout(function() {
-    socket.connect();
-  }, 5000);
+// Connect WebSocket on Service Worker activation
+self.addEventListener('activate', function(event) {
+  connectWebSocket();
 });
 
 // Receive message from client
