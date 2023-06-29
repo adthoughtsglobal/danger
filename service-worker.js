9dad438b49d039f868dd6b5131a3e87319a166bc
adthoughtsglobal <123654929+adthoughtsglobal@users.noreply.github.com>
2023-06-29 13:40:08 +0530

    the betterest

--- service-worker.js
+++ service-worker.js
@@ -1,65 +1,42 @@
-// Establish WebSocket connection
-const socket = new WebSocket('wss://moo.adthoughtsglobal.repl.co');
-
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-      ]);
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  // Close the tab after installation and WebSocket connection
-  self.clients.matchAll().then(function(clients) {
-    clients.forEach(function(client) {
-      client.navigate(client.url);
-      client.postMessage('closeTab');
-    });
+    event.waitUntil(
+      caches.open('my-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/admin.html',
+          '/styles.css',
+          '/admin.js'
+        ]);
+      })
+    );
   });
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// WebSocket event listeners
-socket.addEventListener('open', function() {
-  console.log('WebSocket connection established');
-});
-
-socket.addEventListener('message', function(event) {
-  console.log('WebSocket message received:', event.data);
-
-  // Check for trigger message
-  if (event.data === 'triggerMessage') {
-    // Send trigger received message to the client
-    self.clients.matchAll().then(function(clients) {
-      clients.forEach(function(client) {
-        client.postMessage('triggerReceived');
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
+  self.addEventListener('message', function(event) {
+    if (event.data === 'triggerMessage') {
+      // Connect to the WebSocket
+      const socket = new WebSocket('wss://moo.adthoughtsglobal.repl.co');
+  
+      socket.addEventListener('open', function() {
+        console.log('WebSocket connection established');
+        socket.send('Hello from service worker');
       });
-    });
-  }
-});
-
-socket.addEventListener('close', function() {
-  console.log('WebSocket connection closed. Reconnecting...');
-  // Reconnect after a delay
-  setTimeout(function() {
-    connectWebSocket();
-  }, 5000);
-});
-
-// Receive message from client
-self.addEventListener('message', function(event) {
-  const location = event.data;
-  // Send location data to the server through the WebSocket
-  socket.send(JSON.stringify(location));
-});
+  
+      socket.addEventListener('message', function(event) {
+        console.log('WebSocket message received:', event.data);
+        // Process the received message here
+      });
+  
+      socket.addEventListener('close', function() {
+        console.log('WebSocket connection closed');
+      });
+    }
+  });
+  
